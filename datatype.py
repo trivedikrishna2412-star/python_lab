9bc25c23f78a39e5d5913b74846927f704e0e527
trivedikrishna2412-star <trivedi.krishna2412@gmail.com>
2026-01-06 09:20:32 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,16 @@
+#x="hello world"
+#print(x)
+#a = 30
+#print(a)
+#x = 30.6
+#print(x)
+#x = 2j
+#print(x) 
+#x = ["apple","banana","cherry"]
+#print(x) 
+#x = ("apple","banana","cherry")
+#print(x) 
+#x=frozenset({"apple","banana","cherry"})
+#print(x)
+x=True
+print(x) 
