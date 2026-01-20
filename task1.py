322ee863614f985cea1d2418a4de7fe353d81a53
trivedikrishna2412-star <trivedi.krishna2412@gmail.com>
2026-01-20 08:49:19 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,42 @@
+# Q-1
+p =1000
+r = 5 
+t = 2 
+si = (p * r * t) /100
+print("simple Interest is:",si)
+#Q-2 
+a = 10
+b = 20
+print("Maximum number is:",max(a , b))
+#Q-3
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+#Q-4
+text = "Hello"
+print("length of the string is:",len(text))
+#Q-5
+print("Welcome")
+#Q-6
+s = "Hello"
+print(s[0])
+#Q-7
+s = "Hello"
+print(s[-1])
+#Q-8
+num = int(input("Enter a number: "))
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+#Q-9  
+a = 10
+b = 20
+c = 30 
+print(a + b + c)
+#Q-10
+num = input("enter a value:") 
+print(num)
