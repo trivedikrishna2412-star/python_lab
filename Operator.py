7142dc605631bbf8af1463464d8b11ec758b5a3c
trivedikrishna2412-star <trivedi.krishna2412@gmail.com>
2026-01-27 09:17:44 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,42 @@
+a = 3 
+b = 5
+print(a +b)
+print(a -b)
+print(a *b)
+print(a /b)
+# assignment operator
+x = 10
+print(x)
+# unary operator
+a = 5 
+print(-a)
+# relational operator
+p = 5
+q = 7
+print(p > q)
+print(p == q)
+print(p < q)
+print(p <= q)
+#logical operator
+d = 4
+e = 7
+print(d and e)
+print(d or e)
+print(not d)
+#boolean operator 
+is_pass = True
+print(is_pass)
+#bitwise operator
+f = 5 
+g = 3
+print(f &g)
+print(f | g)
+#membership operator
+num = [1,2,3,4]
+print(2 in num)
+print(5 not in num)
+#identity operator
+s = 10 
+t = 10
+print(s is t)
+print(s is not t)
