76d6be1cafe502eaddda5ce052030784c09b7550
trivedikrishna2412-star <trivedi.krishna2412@gmail.com>
2026-01-31 08:13:35 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,9 @@
+print("hello world")
+name = "riya"
+age = 16
+print("name:",name)
+print("age:",age)
+a = input("enter your name:")
+print("hello",a)
+b = int(input("enter an integer:"))
+print("number",b)
