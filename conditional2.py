401973167ebe9494b16f9486b1ac95317766d4d1
trivedikrishna2412-star <trivedi.krishna2412@gmail.com>
2026-02-14 09:18:32 +0530

    Add files via upload

---
+++ conditional2.py
@@ -0,0 +1,23 @@
+marks = int(input("enter your marks:"))
+if marks >= 90:
+    print("you will get A grade")
+elif marks >= 70:
+    print("you will get B grade")
+else:
+    print("you will get C grade")
+
+
+num = int(input("enter number:"))
+if num % 2 ==0:
+    print("even number")
+else:
+    print("odd number")
+
+
+num = int(input("enter number:"))
+if num > 0:
+    print("positive number")
+elif num<0:
+    print("negative number")
+else:
+    print("Zero")
