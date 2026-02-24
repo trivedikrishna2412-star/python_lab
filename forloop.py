761eecb35e9463498b4ca4224ad47116417b4bbf
trivedikrishna2412-star <trivedi.krishna2412@gmail.com>
2026-02-24 08:19:53 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,27 @@
+#Q-1
+for i in range (1,11):
+    print(i)
+#Q-2
+for i in range (1,21):
+    if i%2==0:
+        print(i)
+#Q-3
+for i in range (1,16):
+    if i%2!=0:
+        print(i)
+#Q-4
+for i in range (1,11):
+    print("5X",i,"=",5*i)
+#Q-5
+name="atmiya"
+for letter in name:
+    print(letter)
+#Q-6
+total=0
+for i in range (1,6):
+    total=total+i
+    print("sum is :",total)
+#Q-7
+numbers= [10,20,30,40]
+for n in numbers:
+    print(n)
