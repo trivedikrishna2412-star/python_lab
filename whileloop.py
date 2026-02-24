ee5562304a510f9db4f694bed17a203d1cf1b965
trivedikrishna2412-star <trivedi.krishna2412@gmail.com>
2026-02-24 08:07:50 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,19 @@
+# Q-1
+i = 1
+while i<=10:
+    print(i)
+    i=i+1
+#Q-2
+n=int(input("enter n:"))
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+    print("sum = ",s)
+#Q-3
+num=int(input("enter number:"))
+i=1
+while i<=10:
+    print(num,"X",i,"=",num*i)
+    i=i+1
