b757474beaa325bd729be59acecf1f8efe3220e5
trivedikrishna2412-star <trivedi.krishna2412@gmail.com>
2026-03-17 09:25:15 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,20 @@
+def student_info(name,age,city):
+    print("name:",name)
+    print("age:",age)
+    print("city:",city)
+student_info(age=18,city="rajkot",name="ravi")
+
+
+def add(a,b,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+display(a=1,c=3,b=2)
+
+
+
+def simple_interest(p:float,r:int,t:float):
+    si=(p*r*t)/100
+    print("simple interest:",si)
+simple_interest(p=10000,t=2.r=1.5)
+simple_interest(t=1.5,p=15000,r=2)
